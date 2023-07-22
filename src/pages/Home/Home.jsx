565639c774f990af3f056a0b5379db4a1b7754a7
jihad-hossain1 <jihadkhan934@gmail.com>
2023-07-22 07:09:31 +0600

    router added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-4xl">Wow i create home page here ...</h3>
+    </div>
+  );
+};
+
+export default Home;
